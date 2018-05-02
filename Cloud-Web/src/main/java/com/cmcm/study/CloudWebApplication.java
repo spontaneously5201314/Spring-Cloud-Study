5211a2315cd7cf448abc385e48ee8ee8b1dc8055
Spontaneously <hongfei@cmcm.com>
2018-05-02 15:41:52 +0800

    添加一个web工程

---
+++ Cloud-Web/src/main/java/com/cmcm/study/CloudWebApplication.java
@@ -0,0 +1,12 @@
+package com.cmcm.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudWebApplication.class, args);
+	}
+}
