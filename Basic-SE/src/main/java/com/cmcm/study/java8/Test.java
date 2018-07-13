3faaf6c07cd9ba68cae4bece208889293acfff53
Spontaneously <hongfei@cmcm.com>
2018-07-13 16:24:38 +0800

    修改Pom文件

---
+++ Basic-SE/src/main/java/com/cmcm/study/java8/Test.java
@@ -0,0 +1,13 @@
+package com.cmcm.study.java8;
+
+import java.util.Date;
+
+/**
+ * @author Spontaneously
+ * @time 2018-07-10 下午8:39
+ */
+public class Test {
+    public static void main(String[] args) {
+//        org.apache.commons.lang3.time.DateUtils.isSameDay(activityData.getModified(), org.apache.commons.lang3.time.DateUtils.addDays(new Date(), -1))
+    }
+}
